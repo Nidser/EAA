18424db9f1e1de48c7b8ad76c2f721cfbbc42af6
nidser <johndiezdaly@gmail.com>
2014-10-10 15:20:33 +0100

    new .sh data.sh

---
+++ week4a/data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#: Title : 
+#: Date : 
+#: Author : 
+#: Version :
+#: Description : 
+#: Options :
+
+# to run the file use: ./fileName.sh
+
+#: Description: print Hello and the first command-line argument 
+printf "Hello, %s%s%s!\n" "$1" " you were born in $2" " and you favorite color is $3"
+
+
