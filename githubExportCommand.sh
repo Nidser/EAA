2098736b32bbbd03b868b9504abac370758dbd7b
nidser <johndiezdaly@gmail.com>
2014-10-10 15:06:08 +0100

    dile deleted & modifiesd to .sh

---
+++ githubExportCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#: Title : Script
+#: Date : 03 Oct 2014
+#: Author : JD
+#: Version :1.0
+#: Description : CPU count 
+#: Options : 
+
+#this will install the standard Exports so we have access tto git hub from the command line
+
+# only run once at the start of each lab
+
+export GIT_SSL_NO_VERIFY=1
+export http_proxy=http://localhost:3128
---
+++ week4/hw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#: Title : 
+#: Date : 
+#: Author : 
+#: Version :
+#: Description : 
+#: Options : 
+
+# to run the file use: ./fileName.sh
+
+#: Description: print Hello and the first command-line argument 
+printf "Hello, %s!\n" "$1"
+
+
